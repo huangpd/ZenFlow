c04e9d2efbcfa096d832e1ca1503d0c5e0de40f0
huangjiajia <1655979823@qq.com>
2026-01-22 21:59:47 +0800

    feat: add HTTP to HTTPS redirect in server.js and update cert paths

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const { createServer } = require('https');
+const { createServer: createHttpsServer } = require('https');
+const { createServer: createHttpServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
 const fs = require('fs');
@@ -9,16 +10,27 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 
 const httpsOptions = {
-    key: fs.readFileSync(path.join(__dirname, 'cloudflare.key')),
-    cert: fs.readFileSync(path.join(__dirname, 'cloudflare_bundle.pem')),
+    key: fs.readFileSync(path.join(__dirname, 'cloudflare_00.key')),
+    cert: fs.readFileSync(path.join(__dirname, 'cloudflare_bundle_00.pem')),
 };
 
 app.prepare().then(() => {
-    createServer(httpsOptions, (req, res) => {
+    // 1. 启动 HTTPS 服务 (443)
+    createHttpsServer(httpsOptions, (req, res) => {
         const parsedUrl = parse(req.url, true);
         handle(req, res, parsedUrl);
     }).listen(443, (err) => {
         if (err) throw err;
-        console.log('> Ready on https://localhost:443');
+        console.log('> HTTPS Ready on https://localhost:443');
+    });
+
+    // 2. 启动 HTTP 服务 (80) 用于自动跳转到 HTTPS
+    createHttpServer((req, res) => {
+        const host = req.headers['host'];
+        res.writeHead(301, { "Location": `https://${host}${req.url}` });
+        res.end();
+    }).listen(80, (err) => {
+        if (err) throw err;
+        console.log('> HTTP Ready on http://localhost:80 (Redirecting to HTTPS)');
     });
 });
